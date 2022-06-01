ae961b8857d06e61ade7e8e4de8781d28c02a087
daniel <daniel.lin.gr@gmail.com>
2022-06-01 15:35:40 +0800

    Update echo_date_to_file.sh

--- Application&Tool/簡單寫檔/echo_date_to_file.sh
+++ Application&Tool/簡單寫檔/echo_date_to_file.sh
@@ -1,6 +1,24 @@
 #!/bin/bash
-today=`date +%Y_%m_%d`
+MMDDhhmm
+today=`date +%Y_%m_%d　%M:%M:％S`
 echo $today
 # 2009_07_21
 # write to file
 echo $date > abc.txt
+
+
+
+# %D – Display date as mm/dd/yy.
+# %Y – Year (e.g., 2020)
+# %m – Month (01-12)
+# %B – Long month name (e.g., November)
+# %b – Short month name (e.g., Nov)
+#　%d – Day of month (e.g., 01)
+#　%j – Day of year (001-366)
+#　%u – Day of week (1-7)
+＃　%A – Full weekday name (e.g., Friday)
+＃　%a – Short weekday name (e.g., Fri)
+＃　%H – Hour (00-23)
+＃　%I – Hour (01-12)
+＃　%M – Minute (00-59)
+＃　%S – Second (00-60)
