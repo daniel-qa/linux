d0e552099d2729d659e078547e18d40590c8fbf0
daniel <daniel.lin.gr@gmail.com>
2023-01-12 18:03:48 +0800

    Create gitpull.sh

---
+++ Application&Tool/spwan/gitpull.sh
@@ -0,0 +1,20 @@
+!/usr/bin/expect
+
+set timeout 30
+cd /home/rd/test/sokradeo
+
+spawn git pull
+expect "Username for 'http://192.168.1.2':"
+send "guest\r"
+send "\r"
+
+expect "Password for 'http://daniel@192.168.1.2:"
+send "guest\r"
+send "\r"
+
+sleep 3
+interact
+
+##
+## call by  'sh test.sh'
+
